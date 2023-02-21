8f3b6e26cae5bb4911dccd7d0964b9c42e7ae828
faulery <yurii.ilnytskyi@gmail.com>
2023-02-21 20:58:34 +0200

    Added ios platform.

--- ionic-app/capacitor.config.ts
+++ ionic-app/capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-app',
+  appId: 'com.app.picture',
+  appName: 'Picture Picker',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
